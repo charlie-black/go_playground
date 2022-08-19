7870daf54b0c93f6e7c63a10957e81c1c38c3429
erastusmugambi <erastus.mugambi@xtranet.co.ke>
2022-08-19 20:14:16 +0300

    model to update notes

--- models/models.go
+++ models/models.go
@@ -10,3 +10,9 @@ type NoteAddParams struct {
 	Title string `json:"title"`
 	Value string `json:"value"`
 }
+
+type NoteUpdateParams struct {
+	ID    int    `db:"id"`
+	Title string `db:"title"`
+	Value string `db:"value"`
+}
