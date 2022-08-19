7f8ff1210da9549ef22a05d7d9a10624f98fa161
erastusmugambi <erastus.mugambi@xtranet.co.ke>
2022-08-19 19:41:31 +0300

    connect to postgesql database

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
-	)
+	"log"
 
-	func main() {
-		fmt.Println("Hello World!")
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	//connect to database (postgres)
+	db, err := sqlx.Connect("postgres", "user=piccasso dbname=notebook sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
 	}
+	println("connected to database", db)
 
-
-
-
-
+}
