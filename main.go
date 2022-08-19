95d701d135e2d4e6c5dccbc94fcbdbff1274312e
erastusmugambi <erastus.mugambi@xtranet.co.ke>
2022-08-19 19:31:44 +0300

    create the main function

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/kataras/iris/v12"
-	_ "github.com/lib/pq"
-	"log"
-	"github.com/jmoiron/sqlx")
+	)
 
-	
+	func main() {
+		fmt.Println("Hello World!")
+	}
+
+
 
 
 
